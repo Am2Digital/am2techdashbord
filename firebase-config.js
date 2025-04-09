1f1b81b6c3a72161d94a954287376b543b62820d
Am2Digital <ahmed.dev@outlook.fr>
2025-04-09 05:53:32 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD3DEpSXS64pvSxOs1sdAZw3Bv81dop00k",
+  authDomain: "am2tech-dashboard.firebaseapp.com",
+  projectId: "am2tech-dashboard",
+  storageBucket: "am2tech-dashboard.firebasestorage.app",
+  messagingSenderId: "708110803770",
+  appId: "1:708110803770:web:4818d748c2af29846ba603",
+  measurementId: "G-3SF6KVG776"
+};
